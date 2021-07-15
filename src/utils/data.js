92bba02c30b737a452541918039173b9f01b09f1
edmilson-dk <droidvidaboa@gmail.com>
2021-07-15 20:59:34 -0300

    feat: created data small

---
+++ src/utils/data.js
@@ -0,0 +1,8 @@
+export const dataSmall = Array(1000).fill().map((val, idx) => {
+  return {
+    id: idx,
+    name: 'John Doe',
+    image: 'http://via.placeholder.com/40',
+    text: "Text text text text text text text"
+  };
+});
